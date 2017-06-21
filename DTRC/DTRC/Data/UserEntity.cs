9c7453579f6766880b2046dd4515565f4e3916f5
Pierluigi Ingrosso <pier_ip@hotmail.it>
2017-06-21 09:00:47 +0000

    Updated UserEntity.cs Signup method

--- DTRC/DTRC/Data/UserEntity.cs
+++ DTRC/DTRC/Data/UserEntity.cs
@@ -46,8 +46,34 @@ namespace DTRC.Data {
             return loginResult;
         }
 
-        public void SignUp(){
+        public void SignUp(Callback signUpCallback){
+            bool signupResult;
+
+            RequestBuilder requestBuilder = new RequestBuilder();
+            Request request = requestBuilder
+                .SetEmailUser(EmailUser)
+                .SetPassUser(PassUser)
+                .Build();
             
+            ServerRequest serverRequest = new ServerRequest();
+
+            string serverResponse = await serverRequest.SendDataToServerAsync(ServerConfig.Instance.server_url_signup, request);
+
+            Response response = ServerResponse.ParsingJsonResponse(serverResponse);
+
+            if (!response.Error) {
+                Debug.WriteLine(string.Format("Registrazione utente avvenuta con successo"));
+                loginResult = true;
+            }
+            else {
+                Debug.WriteLine(string.Format("Il server ha restituito un errore. Messaggio : {0}",
+                        response.Message));
+                loginResult = false;
+            }
+
+            LastErrorMessage = response.Message;
+            loginCallback(loginResult, LastErrorMessage);
+            return loginResult;
         }
 
     }
