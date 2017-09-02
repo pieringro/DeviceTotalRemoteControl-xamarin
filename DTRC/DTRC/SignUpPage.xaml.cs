dadf124295910a815d79b06d00ada650f34c748f
pierprogramm <pierprogramm@gmail.com>
2017-09-02 22:57:06 +0200

    Signup page miglioramento grafico
    Related Work Items: #70

--- DTRC/DTRC/SignUpPage.xaml.cs
+++ DTRC/DTRC/SignUpPage.xaml.cs
@@ -4,23 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace DTRC {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SignUpPage : ContentPage {
+    public partial class SignUpPage : ContentPage, INotifyPropertyChanged {
         public SignUpPage() {
             InitializeComponent();
+            IsWaiting = false;
+            BindingContext = this;
+
             if (this.langPicker.Items.Count == 0) {
                 foreach (string lang in LangEntity.allLang) {
                     this.langPicker.Items.Add(lang);
                 }
+                langPicker.SelectedIndex = 0;
             }
         }
 
         async void OnSignUpButtonClicked(object sender, EventArgs e) {
+            IsWaiting = true;
+
             UserEntity user = new UserEntity {
                 Email = emailEntry.Text,
                 Pass = passwordEntry.Text,
@@ -55,8 +61,31 @@ namespace DTRC {
             else{
                 messageLabel.Text = string.Format("Sign Up failed. Message={0}", user.LastErrorMessage);
             }
+
+            IsWaiting = false;
         }
+
+
+        private bool _isWaiting;
+        public bool IsWaiting {
+            get {
+                return _isWaiting;
+            }
+            set {
+                _isWaiting = value;
+                RaisePropertyChanged("IsWaiting");
+            }
+        }
+
         
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propName) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
 
     }
 }
